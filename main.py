265667542586ec4df6934f449849932909e180ab
Alex Alekseev <granik@ya.ru>
2025-12-19 12:46:06 +0400

    state on 19122025

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyvisa
 from OSCManager import SDS1000CFL
 from GeneratorManager import SDG800
 import time
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -10,9 +11,9 @@ if __name__ == "__main__":
     osc = SDS1000CFL(rm)
     gen = SDG800(rm)
 
-    puls_width = 0.000001 #c
-    puls_amp = 3 #В
-    trig_level = 1 #В
+    puls_width = 0.0000006 #c
+    puls_amp = 1.0 #В
+    trig_level = 0.1 #В
     time_shift = 600 #нс
     time_div = 100 #нс
     vdiv = 2.0 #В
@@ -31,7 +32,7 @@ if __name__ == "__main__":
     gen.reset()
     osc.reset()
 
-    gen.setSignal(t=puls_width,amp=puls_amp)
+    gen.setSignal(t=puls_width,amp=puls_amp/2,offset=puls_amp/4)
     time.sleep(1) 
     
      
@@ -47,15 +48,19 @@ if __name__ == "__main__":
     gen.turnOff()
 
     #while osc.oscilloscope.query('INR?') != '1': pass
- 
-    volt_value2 = osc.getWFdata(2,vdiv2,ofst2)
+    
+    
+    
     volt_value1 = osc.getWFdata(1,vdiv1,ofst1)
+    volt_value2 = osc.getWFdata(2,vdiv2,ofst2)
     
     time_value = osc.calcTIME_value(len(volt_value2),tdiv,sara)
 
     print("Plotting..")
     osc.plotData(time_value,volt_value1,volt_value2,tdiv,trig_level,time_shift,vdiv1,filename)
-    osc.getBMP("screen.bmp")
+    now = datetime.now()  
+    string = now.strftime('%Y_%m_%d_%H_%M_%S')  
+    osc.getBMP("screen" + string + ".bmp")
 
     osc.close()
     gen.close()
