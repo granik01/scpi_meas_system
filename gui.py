6c213fe0e66eaac120bd39452b38727b29c44a3d
Alex Alekseev <granik@ya.ru>
2025-12-23 17:23:38 +0400

    icon is added

--- gui.py
+++ gui.py
@@ -14,7 +14,6 @@ from GeneratorManager import SDG800
 import pyvisa
 import time
 from pathlib import Path
-from tkinter import PhotoImage
 
 class MeasurementApp:
     def __init__(self, root):
@@ -281,9 +280,7 @@ class MeasurementApp:
 
 def main():
     root = tb.Window(themename="darkly")
-    img = PhotoImage(file=Path(__file__).parent/'fav.ico')
-    root.tk.call('wm', 'iconphoto', root._w, img)
-    root.iconbitmap(Path(__file__).parent/'fav.ico')
+    root.iconbitmap(Path(__file__).parent/'osc.ico')
     app = MeasurementApp(root)
     root.mainloop()
 
